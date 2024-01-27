0d44d1193f542ced41fa8f7b18a1210171f3fd0a
Falze Labib <fazle.labib@gmail.com>
2024-01-27 15:48:12 +0600

    Add variables

---
+++ script.js
@@ -0,0 +1,26 @@
+const themeSlider = document.getElementById('slider-range');
+
+const input = document.getElementById('input');
+
+const keySeven = document.getElementById('7');
+const keyEight = document.getElementById('8');
+const keyNine = document.getElementById('9');
+const keyDel = document.getElementById('del');
+
+const keyFour = document.getElementById('4');
+const keyFive = document.getElementById('5');
+const keySix = document.getElementById('6');
+const keyAdd = document.getElementById('+');
+
+const keyOne = document.getElementById('1');
+const keyTwo = document.getElementById('2');
+const keyThree = document.getElementById('3');
+const keySubtract = document.getElementById('-');
+
+const keyDecimalPoint = document.getElementById('.');
+const keyZero = document.getElementById('0');
+const keyDivide = document.getElementById('/');
+const keyMultiply = document.getElementById('x');
+
+const keyReset = document.getElementById('reset');
+const keyResult = document.getElementById('result');
